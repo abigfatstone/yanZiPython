bfb0173469479bc1f20e8bc18027df7cbf694908
xun.jiang <abigfatstone@msn.com>
2020-04-20 01:04:54 +0800

    wordCloud final

--- asciiImage.py
+++ asciiImage.py
@@ -37,36 +37,22 @@ class AsciiImage:
         return fileList   
 
     def ascii_pic(self,inUserSaid):
-
-        #获取程序的根目录
-        cwd = os.getcwd()
         #设置resource文件夹路径
-        resource_dir = os.path.dirname(cwd) +'/resource/'
-        #创建resource文件夹，如果已经存在resource文件夹，该命令会被忽略
-        os.system('mkdir -p ' + resource_dir)
+        resourceDIR = os.path.dirname(os.path.abspath(__file__)) +'/resource/'
 
-        #获取resource文件夹下的所有文件列表
-        fileList = os.listdir(os.path.dirname(resource_dir))
-
-        # #如果是第一次运行，从resource文件夹下把初次照片拷贝到../resource下
-        # if len(fileList) == 0:
-        os.system("cp " + cwd +'/resource/*.* '+ resource_dir)
-        #因为已经拷贝了新文件，所以重新获取文件列表
-        fileList = os.listdir(os.path.dirname(resource_dir))
-        
-        print("如果有新的文件需要转换，请将文件拷贝至文件夹" + resource_dir )
-        print("文件夹" + resource_dir +'当前有以下图片文件：')
+        print("如果有新的图片需要转换，请将图片文件拷贝至文件夹" + resourceDIR )
+        print("文件夹" + resourceDIR +'当前有以下图片文件：')
 
         #初始化文件ID
         fileID = 0
         #由于list的起始角标是0，为了便于理解，对于角标0放入一个空文件名，这样后面新加入的图片文件的编号（角标）可以从1开始。
         fileToAsciiList = ['']
-        for filename in os.listdir(os.path.dirname(resource_dir)):
+        for filename in os.listdir(os.path.dirname(resourceDIR)):
             #遍历所有的文件，如果是图片类型，则将文件放进列表并打印在屏幕上。
             if isimage(filename):
                 fileID = fileID + 1
                 print('[{}]{}'.format(fileID,filename))
-                fileToAsciiList.append(resource_dir + filename)
+                fileToAsciiList.append(resourceDIR + filename)
 
         #要求输入文件编号，并转化为数字类型
         file2AsciiID = eval(input("请输入要转换的文件编号:"))
@@ -94,34 +80,23 @@ class AsciiImage:
      
 
     def worldCloudTxt(self,inUserSaid):
-
-        #获取程序的根目录
-        cwd = os.getcwd()
-        #设置resource文件夹路径
-        resource_dir = os.path.dirname(cwd) +'/resource/'
-
-        if not os.path.exists(resource_dir):
-            #创建resource文件夹，如果已经存在resource文件夹，该命令会被忽略
-            os.system('mkdir -p ' + resource_dir)
-            os.system("cp " + cwd +'/resource/*.* '+ resource_dir)
-
-        print("如果有新的图片文件需要转换，请将图片文件拷贝至文件夹" + resource_dir )
-        print("文件夹" + resource_dir +'当前有以下图片文件：')
+        resourceDIR = os.path.dirname(os.path.abspath(__file__)) +'/resource/'
+        print("如果有新的图片文件需要转换，请将图片文件拷贝至文件夹" + resourceDIR )
+        print("文件夹" + resourceDIR +'当前有以下图片文件：')
         #获取resource文件夹下的所有文件列表
-        imageFileList = self.listFileType(resource_dir,['.jpg','.png'])
+        imageFileList = self.listFileType(resourceDIR,['.jpg','.png'])
         imageFileID = eval(input("请输入要转换的文件编号:"))
 
-        print("如果有新的txt文件需要转换，请将txt文件拷贝至文件夹" + resource_dir )
-        print("文件夹" + resource_dir +'当前有以下txt文件：')
+        print("如果有新的txt文件需要转换，请将txt文件拷贝至文件夹" + resourceDIR )
+        print("文件夹" + resourceDIR +'当前有以下txt文件：')
         #获取resource文件夹下的所有文件列表
-        txtFileList = self.listFileType(resource_dir,['.txt','.py'])
+        txtFileList = self.listFileType(resourceDIR,['.txt','.py'])
         txtFileID = eval(input("请输入要转换的文件编号:"))
 
         # 生成对象
         mask = np.array(Image.open(imageFileList[imageFileID]))
         # 从图片中生成颜色
         image_colors = ImageColorGenerator(mask)
-
 
         text = open(txtFileList[txtFileID]).read()
         # 中文分词  
@@ -155,9 +130,11 @@ class AsciiImage:
         # plt.show()
          
         # 保存到文件
-        os.system('mkdir -p ' + os.path.dirname(cwd) +'/output/')
-        wc.to_file(os.path.dirname(cwd) +'/output/'+'wordCloudOut.png')
-        return ['list_function',"生成的文件为："+os.path.dirname(cwd) +'/output/'+'wordCloudOut.png','0']
+        homeDIR = os.path.dirname(os.path.abspath(__file__)) 
+        fileName = os.path.split(imageFileList[imageFileID])[1]
+        os.system('mkdir -p ' + homeDIR +'/../output/')
+        wc.to_file(homeDIR +'/../output/out.'+fileName)
+        return ['list_function',"生成的文件为："+homeDIR +'/../output/out.'+fileName,'0']
 
 if __name__ == "__main__":
     asciiImage = AsciiImage()
--- firstScript.py
+++ firstScript.py
@@ -4,28 +4,55 @@ from helloWorld import HelloWorld
 from clockAngle import ClockAngle
 from turtleDraw import TurtleDraw
 from asciiImage import AsciiImage
-from chatbotWeb import ChatbotWeb
+import os
+# from chatbotWeb import chatbotWeb  #默认不调用ai聊天
 
 class Chatbot:
 
     class RunMode:
         INTERACTIVE = 'interactive'
         DAEMON ='daemon'
-
+    def copyResource(self):
+        ########################
+        #拷贝资源文件
+        ########################
+        #获取程序的根目录
+        cwd = os.path.abspath(__file__)
+        homeDIR = os.path.dirname(cwd)
+        #设置resource文件夹路径
+        resourceDIR = homeDIR +'/../resource/'
+        #创建resource文件夹，如果已经存在resource文件夹，该命令会被忽略
+        os.system('mkdir -p ' + resourceDIR)
+        #获取自带resource文件夹下的所有文件列表
+        fileList = os.listdir(homeDIR +'/resource/')
+        for file in fileList:
+            if not os.path.exists(resourceDIR + '/' + file):
+                os.system("cp " + homeDIR +'/resource/'+ file +' ' + resourceDIR)
+                
     def __init__(self):
         # Model/dataset parameters
         self.args = None
+        self.copyResource()
+
+        #设置系统变量
         self.SENTENCES_PREFIX = ['AI: ', 'yanZi: ']
         self.callbackKey = 'first_call'
         self.stepID = '0'
-        self.is_debug = '0'
-        self.functionList = '输入“list”，打印功能列表\n输入编号启动相应的问题\n[1]启动helloWorld\n[2]启动钟表问题\n[3]计算钟表角度\n[4]打印9X9乘法表\n[5]画太阳花\n' +\
-                            '[6]画五角星\n[7]画多边形\n[8]画狮子\n[9]画小猪佩奇\n[10]画名字\n[11]照片转化ASCII\n[12]照片转wordCloud'
+        self.isDebug = False
+        self.isChat = False
+        self.functionList = '输入“list”，打印功能列表\n输入编号启动相应的问题\n' + \
+                            '[1]启动helloWorld\n[2]计算钟表角度\n[3]计算时钟分钟夹角\n[4]打印9X9乘法表\n' + \
+                            '[5]画太阳花\n[6]画五角星\n[7]画多边形\n[8]画狮子\n' + \
+                            '[9]画小猪佩奇\n[10]画名字\n[11]照片转化ASCII\n[12]照片转wordCloud'
+
+        #初始化
         self.helloWorld = HelloWorld()
         self.clockAngle = ClockAngle()
         self.turtleDraw = TurtleDraw()
         self.asciiImage = AsciiImage()
-        self.chatbotWeb = ChatbotWeb()
+        # if self.isChat:
+        #     self.chatbotWeb = ChatbotWeb()
+
 
     def sayHello(self):
         helloString = '姜彦孜你好，接下来将开始你的python奇妙之旅\n'+self.functionList
@@ -38,13 +65,23 @@ class Chatbot:
         self.stepID ='0'
         return [self.callbackKey,self.functionList,self.stepID]
 
+    def chatAsBackEnd(self,inUserInput):
+        #性能考虑，默认关闭ai聊天，返回功能列表
+        # if self.isChat:
+        #     aiSaid = self.chatbotWeb.chatWithBot(inUserInput)
+        # else:
+        #     aiSaid = self.listFunction()
+        aiSaid = self.listFunction()    
+        return aiSaid
+
     def daemonPredict(self, inUserSaid):
 
-        if self.is_debug == '1':
+        if self.isDebug:
             print([inUserSaid,self.callbackKey,self.stepID,"start daemonPredict"])
-
+        #如果用户输入list，则session状态重置，打印功能列表
         if inUserSaid == 'list':
             aiSaid = self.listFunction()
+        #如果用户是在等待功能列表状态，则判断启动哪个功能项    
         elif self.callbackKey == 'list_function':
             if inUserSaid == "1":
                 aiSaid = self.helloWorld.printHelloWorld([inUserSaid,'0'])
@@ -71,16 +108,15 @@ class Chatbot:
             elif inUserSaid == '12' :
                 aiSaid = self.asciiImage.worldCloudTxt([inUserSaid,'0'])                                                          
             else:
-                aiSaid = self.chatbotWeb.chatWithBot([inUserSaid,self.stepID])
-
+                #如果不是已知功能列表，则调用聊天程序
+                aiSaid = self.chatAsBackEnd([inUserSaid,'0'])
         elif self.callbackKey =='calc_clock_angle':
             aiSaid = self.clockAngle.calcClockAngle([inUserSaid,self.stepID])
-
         elif self.callbackKey =='calc_hour_minute':
             aiSaid = self.clockAngle.calcHourMinute([inUserSaid,self.stepID])
-
         else:
-            aiSaid = self.chatbotWeb.chatWithBot([inUserSaid,self.stepID])
+            #如果不是已知状态，则调用聊天程序
+            aiSaid = self.chatAsBackEnd([inUserSaid,'0'])
 
         self.callbackKey = aiSaid[0]
         self.stepID = aiSaid[2]
@@ -89,13 +125,16 @@ class Chatbot:
 
 
     def chatInteractive(self):
+        #初次启动，打印欢迎语
         aiSaid = self.sayHello()
         print('{}{}'.format(self.SENTENCES_PREFIX[0],aiSaid[1]))
         
         while True:
             userSaid = input(self.SENTENCES_PREFIX[1])
-            if userSaid.lower() == 'exit':    
+            if userSaid.lower() == 'exit':   
+                #退出的时候关闭打开的turtle窗口，否则窗口会一直在
                 self.turtleDraw.turtleClose()
+                #退出程序
                 break
             
             aiSaid = self.daemonPredict(userSaid)
