54cb13afc36b20c3db775b790a7a87cd46f4a08f
abigfatstone <abigfatstone@msn.com>
2020-05-17 07:36:08 +0800

    settings for asgi

--- botWeb/botWeb/settings.py
+++ botWeb/botWeb/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+# https://www.cnblogs.com/zepc007/p/12030913.html
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y2zuw(r#*&y@q^zd9i5bz*hsjf1%jb28erv4u%=1o7-gdh*fl8'
+SECRET_KEY = 'CHATBOT_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'botWeb_interface'
-]
+    'botWeb_interface',
+    'channels', #注册app
+]   
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -99,6 +100,19 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+redis_url = os.environ.get('CHATBOT_REDIS_URL', 'localhost')
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('localhost', 6379)], 
+        },
+        "ROUTING": "chatbot_interface.routing.channel_routing",   
+    },
+}
 
 
 # Internationalization
