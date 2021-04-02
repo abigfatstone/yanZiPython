1397cbf00f3e91581cc11d0dbdf2163075ff3532
abigfatstone <abigfatstone@msn.com>
2021-04-03 07:46:45 +0800

    get file size

--- lesson/rename_pic.py
+++ lesson/rename_pic.py
@@ -1,12 +1,48 @@
 import os
 import subprocess,time
 import pytz
+import hashlib
+
+
 
 def os_cmd(cmd,is_debug=False):
     if is_debug:
         print(cmd)
     else:
         os.system(cmd)
+
+def getDocSize(path):
+    try:
+        size = os.path.getsize(path)
+        return formatSize(size)
+    except Exception as err:
+        print(err)
+
+        
+def get_md5_01(file_path):
+  md5 = None
+  if os.path.isfile(file_path):
+    f = open(file_path,'rb')
+    md5_obj = hashlib.md5()
+    md5_obj.update(f.read())
+    hash_code = md5_obj.hexdigest()
+    f.close()
+    md5 = str(hash_code).lower()
+  return md5
+
+def get_md5_02(file_path):
+  f = open(file_path,'rb') 
+  md5_obj = hashlib.md5()
+  while True:
+    d = f.read(8096)
+    if not d:
+      break
+    md5_obj.update(d)
+  hash_code = md5_obj.hexdigest()
+  f.close()
+  md5 = str(hash_code).lower()
+  return md5
+ 
 
 def get_from_pohto(file_path):
     import exifread 
